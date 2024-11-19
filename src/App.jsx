2f6c325dbe24faebf7b2ca55933d5252a86ab6da
Jomar-Navarro <jomarnavarro201@gmail.com>
2024-11-19 17:37:31 +0100

    [add] fix header

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,18 @@ import Header from "./assets/components/Header";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
-	{ path: "/", element: <Home /> },
-	{ path: "/about", element: <About /> },
-	{ path: "/projects", element: <Projects /> },
+	{
+		path: "/",
+		element: <RootLayout />,
+		children: [
+			{ index: true, element: <Home /> },
+			{ path: "/about", element: <About /> },
+			{ path: "/projects", element: <Projects /> },
+		],
+	},
 ]);
 
 function App() {
