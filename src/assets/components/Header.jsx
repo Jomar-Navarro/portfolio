4e47334920f868c4a5a293855c213e200a218eea
Jomar-Navarro <jomarnavarro201@gmail.com>
2024-11-21 17:39:37 +0100

    [add] toggle button not styled yet

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -3,14 +3,14 @@ import { NavLink } from "react-router-dom";
 export default function Header() {
 	return (
 		<div className="d-flex justify-content-center my-5">
-			<nav className="d-flex justify-content-between align-items-center w-100 px-4 py-3">
+			<nav className="d-flex justify-content-between align-items-center w-100 px-4 py-3 rounded-5 border-2">
 				<h3 className="text-white">Jomar Navarro</h3>
 				<div>
 					<ul className="nav">
 						<li className="nav-item ">
 							<NavLink
 								to="/"
-								className="nav-link text-white"
+								className="nav-link text-white fs-5"
 								activeClassName="active"
 							>
 								Home
@@ -19,7 +19,7 @@ export default function Header() {
 						<li className="nav-item">
 							<NavLink
 								to="/about"
-								className="nav-link text-white"
+								className="nav-link text-white fs-5"
 								activeClassName="active"
 							>
 								About Me
@@ -28,7 +28,7 @@ export default function Header() {
 						<li className="nav-item">
 							<NavLink
 								to="/projects"
-								className="nav-link text-white"
+								className="nav-link text-white fs-5"
 								activeClassName="active"
 							>
 								Projects
@@ -37,8 +37,17 @@ export default function Header() {
 					</ul>
 				</div>
 
-				<div>
-					<button>Contact Me!</button>
+				<div className="d-flex align-items-center gap-3">
+					{/* Bottone per Dark Mode */}
+					<div>
+						<input type="checkbox" className="checkbox" id="checkbox" />
+						<label htmlFor="checkbox" className="checkbox-label">
+							<i className="fas fa-moon"></i>
+							<i className="fas fa-sun"></i>
+							<span className="ball"></span>
+						</label>
+					</div>
+					<button className="btn btn-success rounded-4 p-3">Contact Me!</button>
 				</div>
 			</nav>
 		</div>
