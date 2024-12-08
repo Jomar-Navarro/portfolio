fc131256c938e4186646cc328296a201a3ec09eb
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-12-08 15:18:01 +0100

    [add] style on image

--- src/assets/components/Hero.jsx
+++ src/assets/components/Hero.jsx
@@ -3,13 +3,13 @@ import styles from "../style/Hero.module.scss";
 export default function Hero() {
 	return (
 		<>
-			<div className="container">
-				<div className="d-flex justify-content-center">
+			<div className="container py-5">
+				<div className="d-flex justify-content-center text-center text-white">
 					<div className={styles.title}>
-						<h1 className="text-white text-center">
-							Junior Full-Stack Web Developer
-						</h1>
+						<h1>Junior Full-Stack Web Developer</h1>
+						<p>Sometext</p>
 					</div>
+
 					<div className={styles.image}>
 						<img src="src/assets/img/self.PNG" alt="myself" />
 					</div>
