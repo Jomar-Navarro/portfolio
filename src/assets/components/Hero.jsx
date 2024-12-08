99dcc4967ce4df9d2a5b77c9b8661fada182ac20
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-12-08 15:52:43 +0100

    [add] description

--- src/assets/components/Hero.jsx
+++ src/assets/components/Hero.jsx
@@ -7,7 +7,13 @@ export default function Hero() {
 				<div className="d-flex justify-content-center text-center text-white">
 					<div className={styles.title}>
 						<h1>Junior Full-Stack Web Developer</h1>
-						<p>Sometext</p>
+
+						<p>
+							Sono Jomar Navarro, Full Stack Web Developer. Specializzato nella
+							creazione di applicazioni web moderne, intuitive e scalabili.
+							Esplora i miei progetti e scopri come posso dare vita alle tue
+							idee!
+						</p>
 					</div>
 
 					<div className={styles.image}>
