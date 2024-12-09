5041441f467d33e464410624a4929fd0002de262
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-12-09 20:25:08 +0100

    [add] card style

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,13 +5,17 @@ export default function Projects() {
 		<>
 			<h1 className="text-center text-white">Welcome to my Projects!</h1>
 
-			<div className="d-flex">
-				<div className={styles.card}>
-					<img src="src/assets/img/logo/html-logo.svg" alt="" />
+			<div className="justify-content-center">
+				<div className="right d-flex">
+					<div className={styles.card}>
+						<img src="src/assets/img/logo/html-logo.svg" alt="" />
+					</div>
 				</div>
 
-				<div className={styles.card}>
-					<img src="src/assets/img/logo/" alt="" />
+				<div className="left d-flex flex-row-reverse">
+					<div className={styles.card}>
+						<img src="src/assets/img/logo/css-logo.svg" alt="" />
+					</div>
 				</div>
 			</div>
 		</>
