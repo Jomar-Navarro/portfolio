6eac09a0023919944faf677ae3b687e6fd1287fe
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-12-09 20:19:57 +0100

    [add] Projects card

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,3 +1,19 @@
+import styles from "../assets/style/Project.module.scss";
+
 export default function Projects() {
-	return <h1 className="text-center text-white">Welcome to my Projects!</h1>;
+	return (
+		<>
+			<h1 className="text-center text-white">Welcome to my Projects!</h1>
+
+			<div className="d-flex">
+				<div className={styles.card}>
+					<img src="src/assets/img/logo/html-logo.svg" alt="" />
+				</div>
+
+				<div className={styles.card}>
+					<img src="src/assets/img/logo/" alt="" />
+				</div>
+			</div>
+		</>
+	);
 }
