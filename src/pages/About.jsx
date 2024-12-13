11934265c6a98e752f5447b44eb7d63520ef7124
Jomar-Navarro <jomarnavarro201@gmail.com>
2024-12-13 16:21:42 +0100

    [add] image & description

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,25 @@
+import styles from "../assets/style/About.module.scss";
+
 export default function About() {
-	return <h1>About me!</h1>;
+	return (
+		<>
+			<div className="container d-flex justify-content-center align-items center mx-5">
+				<div className={styles.image}>
+					<h1>About Me!</h1>
+					<img src="src/assets/img/about-img.jpg" alt="About Me" />
+				</div>
+
+				<div className={styles.description}>
+					<p>
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas,
+						nihil laudantium voluptatum inventore reiciendis ab dolorem porro
+						aliquid! Sed a nobis non sequi modi, amet alias omnis! Vitae
+						blanditiis numquam voluptas recusandae cumque neque illo culpa,
+						necessitatibus dolores minima odio pariatur impedit optio id.
+						Ducimus asperiores iure atque quae distinctio?
+					</p>
+				</div>
+			</div>
+		</>
+	);
 }
