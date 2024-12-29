3bd598c4e42200b560b0b1db8c1767c52c2c0068
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-12-29 17:43:25 +0100

    feat: create ProjectsPage component to encapsulate project display logic and improve code organization

---
+++ src/assets/components/ProjectsPage.jsx
@@ -0,0 +1,10 @@
+export default function ProjectsPage() {
+	return (
+		<>
+			<div className="container text-center text-white">
+				<h1>Welcome to My Projects</h1>
+				<p>Here's all the projects that i've worked on.</p>
+			</div>
+		</>
+	);
+}
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,63 +1,9 @@
-import { useEffect, useState } from "react";
-import styles from "../assets/style/Project.module.scss";
-import Card from "../assets/components/Card";
-import ProjectsApi from "../data/ProjectsAPI"; // Funzione API
-import Paginator from "../assets/components/Paginator";
+import ProjectsPage from "../assets/components/ProjectsPage";
 
 export default function Projects() {
-	const [projects, setProjects] = useState([]);
-	const [loading, setLoading] = useState(true);
-	const [currentPage, setCurrentPage] = useState(1);
-	const [projectsPerPage] = useState(6);
-
-	useEffect(() => {
-		ProjectsApi()
-			.then((data) => {
-				setProjects(data);
-				setLoading(false);
-			})
-			.catch((error) => {
-				console.error("Errore nel caricamento dei progetti:", error);
-				setLoading(false);
-			});
-	}, []);
-
-	if (loading) {
-		return <p className={styles.loading}>Caricamento progetti...</p>;
-	}
-
-	// Calcolo dei progetti da visualizzare
-	const indexOfLastProject = currentPage * projectsPerPage;
-	const indexOfFirstProject = indexOfLastProject - projectsPerPage;
-	const currentProjects = projects.slice(
-		indexOfFirstProject,
-		indexOfLastProject
-	);
-
 	return (
 		<>
-			<div className="container">
-				<h1 className={styles.title}>Welcome to my Projects!</h1>
-				<div className={styles.wrapper}>
-					{/* Elementi decorativi */}
-					<div className={styles.deco}></div>
-					<div className={styles.decoTwo}></div>
-					<div className={styles.decoThree}></div>
-					{/* {/* Elementi decorativi */}
-					{currentProjects.map((project, index) => (
-						<div key={index}>
-							<Card project={project} url={project.url} />
-						</div>
-					))}
-				</div>
-
-				<Paginator
-					currentPage={currentPage}
-					totalItems={projects.length}
-					itemsPerPage={projectsPerPage}
-					onPageChange={(page) => setCurrentPage(page)}
-				/>
-			</div>
+			<ProjectsPage />
 		</>
 	);
 }
