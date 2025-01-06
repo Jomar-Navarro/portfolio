7a41665df429e33f4ae012e090c64042bf241731
Jomar-Navarro <Jomarnavarro201@gmail.com>
2025-01-06 19:39:42 +0100

    feat: update font styles across the application and enhance AboutSection layout

--- src/assets/components/AboutSection.jsx
+++ src/assets/components/AboutSection.jsx
@@ -4,16 +4,8 @@ export default function AboutPage() {
 	return (
 		<>
 			<div className="d-flex justify-content-center align-items center mx-5 my-5">
-				<div className={styles.image}>
-					<h1 className="text-white">
-						About <br />
-						me
-					</h1>
-					<img src="src/assets/img/about.svg" alt="About Me" />
-				</div>
-
 				<div className={styles.description}>
-					<p className="d-flex align-items-center justify-content-center h-100 text-white pe-5 mx-5">
+					<p className="d-flex align-items-center flex-column justify-content-center h-100 text-white pe-5 mx-5">
 						Ciao! Sono Jomar, uno sviluppatore web full-stack appassionato di
 						tecnologia e design. Con esperienza in linguaggi come JavaScript,
 						Vue.js e React, amo trasformare idee in soluzioni digitali eleganti
@@ -24,9 +16,19 @@ export default function AboutPage() {
 						determinazione a migliorarmi ogni giorno. Cerco costantemente nuove
 						sfide per crescere professionalmente e contribuire a progetti
 						innovativi. Quando non sono davanti al computer, mi alleno e sogno
-						di costruire un'AI personale come Jarvis di Iron Man. Ti va di
-						lavorare insieme?
+						di costruire un'AI personale come Jarvis di Iron Man.
+						<span className="pt-5 text-start w-100 fs-3">
+							Ti va di lavorare insieme?
+						</span>
 					</p>
+				</div>
+
+				<div className={styles.image}>
+					<h1 className="text-white text-end">
+						About <br />
+						me
+					</h1>
+					<img src="src/assets/img/about-section.png" alt="About Me" />
 				</div>
 			</div>
 		</>
