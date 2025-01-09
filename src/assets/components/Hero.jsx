cc6efc97432ae28803739aa385330870eeb1a4a1
Jomar-Navarro <Jomarnavarro201@gmail.com>
2025-01-09 20:47:00 +0100

    feat: add SplitText component for animated text display and integrate into Hero component

--- src/assets/components/Hero.jsx
+++ src/assets/components/Hero.jsx
@@ -1,4 +1,5 @@
 import Swiper from "./Swiper";
+import SplitText from "./SplitText";
 
 import styles from "../style/Hero.module.scss";
 
@@ -9,9 +10,20 @@ export default function Hero() {
 				<div className="container">
 					<div className="d-flex justify-content-center text-center text-white">
 						<div className={styles.title}>
-							<h1>Junior Full-Stack Web Developer</h1>
-
-							<p>
+							<SplitText
+								text="Junior Full Stack Web Developer"
+								className={styles.mainTitle}
+								delay={50}
+								animationFrom={{
+									opacity: 0,
+									transform: "translate3d(0,50px,0)",
+								}}
+								animationTo={{ opacity: 1, transform: "translate3d(0,0,0)" }}
+								easing="easeOutCubic"
+								threshold={0.2}
+								rootMargin="-50px"
+							/>
+							<p className={styles.subtitle}>
 								Sono Jomar Navarro, Full Stack Web Developer. Specializzato
 								nella creazione di applicazioni web moderne e intuitive. Esplora
 								i miei progetti e scopri come posso dare vita alle tue idee!
