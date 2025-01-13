53af967896e9ccb8ad98fb8a48ceb8063e53e359
Jomar-Navarro <Jomarnavarro201@gmail.com>
2025-01-13 19:48:48 +0100

    feat: integrate react-slick for carousel functionality in Swiper component

--- src/assets/components/Swiper.jsx
+++ src/assets/components/Swiper.jsx
@@ -1,32 +1,34 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules";
-import "swiper/css"; // Importa i CSS generali di Swiper
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import styles from "../style/Swiper.module.scss";
 
 import logos from "../../data/logos";
 
 export default function App() {
+	const settings = {
+		centerMode: true,
+		slidesToShow: 5,
+		autoplay: true,
+		autoplaySpeed: 0,
+		speed: 2500,
+		cssEase: "linear",
+		infinite: true,
+		pauseOnHover: false,
+		arrows: false,
+		rtl: true,
+	};
+
 	return (
-		<Swiper
-			centeredSlides={true}
-			slidesPerView={5}
-			loop={true}
-			speed={1500}
-			autoplay={{
-				delay: 0, // Imposta il tempo in millisecondi per ogni slide
-				disableOnInteraction: true, // Continua l'autoplay anche dopo l'interazione
-			}}
-			modules={[Autoplay]}
-			className={styles.swiper}
-		>
-			{logos.map((logo, index) => (
-				<SwiperSlide key={index}>
-					<div className={styles.logo}>
-						<img src={logo.src} alt={logo.alt} />
+		<div className={styles.mySwiper}>
+			<Slider {...settings} className={styles.swiper}>
+				{logos.map((logos, index) => (
+					<div key={index} className={styles.logo}>
+						<img src={logos.src} alt={logos.alt} />
 					</div>
-				</SwiperSlide>
-			))}
-		</Swiper>
+				))}
+			</Slider>
+		</div>
 	);
 }
