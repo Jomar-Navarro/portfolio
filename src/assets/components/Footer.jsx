74561ff177ac9188cccf9b317d2a045386f51587
Jomar-Navarro <Jomarnavarro201@gmail.com>
2025-01-16 10:12:51 +0100

    feat: enhance Footer component layout and styling for improved presentation

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -1,52 +1,50 @@
 import { NavLink } from "react-router-dom";
 
+import "../style/Footer.module.scss";
+
 export default function Footer() {
 	return (
 		<>
-			<div className="container py-5">
-				<div className="row justify-content-center">
-					<div className="col-md-8">
-						<div className="row text-white">
-							<div className="col-md-6">
-								<h3>Titolo</h3>
-								<ul className="list-unstyled d-flex align-items-center justify-content-start fs-1">
-									<li>
-										<a href="#">
-											<i class="fa-brands fa-linkedin"></i>
-										</a>
-									</li>
-									<li className="mx-4">
-										<a href="#">
-											<i class="fa-brands fa-square-github"></i>
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<i class="fa-solid fa-download"></i>
-										</a>
-									</li>
-								</ul>
-								<p>© 2025 Jomar Navarro. All rights reserved</p>
-							</div>
+			<div className={`container py-5`}>
+				<div className="row  justify-content-center text-white">
+					<div className="col-md-6">
+						<h3>Jomar Navarro</h3>
+						<ul className="list-unstyled d-flex align-items-center justify-content-start fs-1">
+							<li>
+								<a href="#">
+									<i class="fa-brands fa-linkedin"></i>
+								</a>
+							</li>
+							<li className="mx-4">
+								<a href="#">
+									<i class="fa-brands fa-square-github"></i>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<i class="fa-solid fa-download"></i>
+								</a>
+							</li>
+						</ul>
+						<p>© 2025 Jomar Navarro. All rights reserved</p>
+					</div>
 
-							<div className="col-md-3">
-								<h3>Contatti</h3>
-								<ul className="list-unstyled">
-									<li>Indirizzo</li>
-									<li>Telefono</li>
-									<li>Email</li>
-								</ul>
-							</div>
+					<div className="col-md-3">
+						<h3>Contatti</h3>
+						<ul className="list-unstyled">
+							<li>Indirizzo</li>
+							<li>Telefono</li>
+							<li>Email</li>
+						</ul>
+					</div>
 
-							<div className="col-md-3">
-								<h3>RoadMap</h3>
-								<ul className="list-unstyled d-flex flex-column">
-									<NavLink to="/">Home</NavLink>
-									<NavLink to="/about">About</NavLink>
-									<NavLink to="/projects">Projects</NavLink>
-								</ul>
-							</div>
-						</div>
+					<div className="col-md-3">
+						<h3>RoadMap</h3>
+						<ul className="list-unstyled d-flex flex-column">
+							<NavLink to="/">Home</NavLink>
+							<NavLink to="/about">About</NavLink>
+							<NavLink to="/projects">Projects</NavLink>
+						</ul>
 					</div>
 				</div>
 			</div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import Hero from "../assets/components/Hero";
 import About from "../assets/components/AboutSection";
 import BestProjects from "../assets/components/bestProjects";
-import Footer from "../assets/components/Footer";
 
 export default function Home() {
 	return (
