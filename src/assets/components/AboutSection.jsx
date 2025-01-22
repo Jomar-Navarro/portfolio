a25a1b30055c086f7df430d30bcd5da7e7350f5e
Jomar-Navarro <Jomarnavarro201@gmail.com>
2025-01-22 15:30:25 +0100

    feat: improve layout and styles for AboutSection component; enhance responsiveness and structure

--- src/assets/components/AboutSection.jsx
+++ src/assets/components/AboutSection.jsx
@@ -6,46 +6,46 @@ import Button from "./Button";
 export default function AboutPage() {
 	return (
 		<>
-			<div
-				className={`${styles.wrapper} d-flex justify-content-center align-items-center`}
-			>
-				<div className={styles.description}>
-					<div className="d-flex flex-column h-100 text-white">
-						<p className="p-0 m-0">
-							Ciao! Sono Jomar, uno sviluppatore web full-stack appassionato di
-							tecnologia e gadget elettronici con esperienza in diversi
-							linguaggi tecnologici. Mi definiscono la mia curiosità e la mia
-							competitività. Cerco costantemente nuove sfide per crescere
-							professionalmente e contribuire a progetti innovativi.
-						</p>
-						<p className="p-0 m-0">
-							<NavLink to="/about" className="text-white text-end">
-								Scopri di più su di me...
-							</NavLink>
-						</p>
+			<div className={`${styles.wrapper}`}>
+				<div className="container-fluid d-flex justify-content-center align-items-center">
+					<div className={`${styles.description}`}>
+						<div className="d-flex flex-column h-100 text-white mx-5 px-5">
+							<p className="p-0 m-0">
+								Ciao! Sono Jomar, uno sviluppatore web full-stack appassionato
+								di tecnologia e gadget elettronici con esperienza in diversi
+								linguaggi tecnologici. Mi definiscono la mia curiosità e la mia
+								competitività. Cerco costantemente nuove sfide per crescere
+								professionalmente e contribuire a progetti innovativi.
+							</p>
+							<p className="p-0 m-0">
+								<NavLink to="/about" className="text-white text-end">
+									Scopri di più su di me...
+								</NavLink>
+							</p>
 
-						<div className="pt-4 d-flex flex-column align-items-end">
-							<span className="text-start w-100 fs-3 text-end fw-bolder">
-								Ti va di lavorare insieme?
-							</span>
-							<div className={styles.download}>
-								<p className="pe-3">Scarica il mio CV</p>
-								<Button
-									href="public\CV\JOMAR_NAVARRO_CV_IT.pdf"
-									download="Jomar_Navarro_CV.pdf"
-								/>
+							<div className="pt-4 d-flex flex-column align-items-end">
+								<span className="text-start w-100 fs-3 text-end fw-bolder">
+									Ti va di lavorare insieme?
+								</span>
+								<div className={styles.download}>
+									<p className="pe-3">Scarica il mio CV</p>
+									<Button
+										href="public\CV\JOMAR_NAVARRO_CV_IT.pdf"
+										download="Jomar_Navarro_CV.pdf"
+									/>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
 
-				<div className={styles.image}>
-					<h3 className="text-white text-end">
-						Su di
-						<br />
-						me
-					</h3>
-					<img src="src/assets/img/about-section.png" alt="About Me" />
+					<div className={styles.image}>
+						<h3 className="text-white text-end">
+							Su di
+							<br />
+							me
+						</h3>
+						<img src="src/assets/img/about-section.png" alt="About Me" />
+					</div>
 				</div>
 			</div>
 		</>
