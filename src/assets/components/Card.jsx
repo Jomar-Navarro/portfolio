504dd68a01cedf6279a560178570b2f1855b4a8c
Jomar-Navarro <Jomarnavarro201@gmail.com>
2025-01-27 19:40:14 +0100

    feat: implement i18next translations for Card component in English and Italian

--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -1,23 +1,26 @@
 import styles from "../style/Card.module.scss";
 import dayjs from "dayjs";
 import "dayjs/locale/it"; // Importa la localizzazione italiana
+import { useTranslation } from "react-i18next";
 
 dayjs.locale("it"); // Imposta la localizzazione italiana
 
 export default function Card({ project, url }) {
+	const { t } = useTranslation(); // Hook per la traduzione
+
 	return (
 		<div className={`${styles.wrapper}`}>
 			<div className={`${styles.card}`}>
 				<a className={styles.link} href={url} target="_blank" rel="noreferrer">
 					<p className={styles.heading}>{project.title}</p>
-					<p>Made By</p>
+					<p>{t("card.madeBy")}</p>
 					<p>Jomar Navarro</p>
 
 					<div className={styles.descriptions}>
 						<p>
 							{project.created_at
 								? dayjs(project.created_at).format("DD/MM/YYYY")
-								: "No date available"}
+								: t("card.noDateAvailable")}
 						</p>
 					</div>
 				</a>
