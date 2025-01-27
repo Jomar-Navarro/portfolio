42dc3409604c68c65c5df1f03f270d46d4f0fa02
Jomar-Navarro <Jomarnavarro201@gmail.com>
2025-01-27 19:28:56 +0100

    feat: implement i18next translations for AboutSection component in English and Italian

--- src/assets/components/AboutSection.jsx
+++ src/assets/components/AboutSection.jsx
@@ -1,34 +1,31 @@
 import { NavLink } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 import styles from "../style/AboutSection.module.scss";
 import Button from "./Button";
 
 export default function AboutPage() {
+	const { t } = useTranslation(); // Hook per la traduzione
+
 	return (
 		<>
 			<div className="container-fluid container-lg d-flex justify-content-center align-items-center px-5">
 				<div className={`${styles.wrapper}`}>
 					<div className={`${styles.description}`}>
 						<div className="d-flex flex-column h-100 text-white  justify-content-center">
-							<p className="p-0 m-0">
-								Ciao! Sono Jomar, uno sviluppatore web full-stack appassionato
-								di tecnologia e gadget elettronici con esperienza in diversi
-								linguaggi tecnologici. Mi definiscono la mia curiosità e la mia
-								competitività. Cerco costantemente nuove sfide per crescere
-								professionalmente e contribuire a progetti innovativi.
-							</p>
+							<p className="p-0 m-0">{t("aboutSection.greeting")}</p>
 							<p className="p-0 m-0">
 								<NavLink to="/about" className="text-white text-end">
-									Scopri di più su di me...
+									{t("aboutSection.learnMore")}
 								</NavLink>
 							</p>
 
 							<div className="pt-4 d-flex flex-column align-items-start">
 								<span className="w-100 fs-3 fw-bolder">
-									Ti va di lavorare insieme?
+									{t("aboutSection.workTogether")}
 								</span>
 								<div className={styles.download}>
-									<p className="pe-3">Scarica il mio CV</p>
+									<p className="pe-3">{t("aboutSection.downloadCV")}</p>
 									<Button
 										href="/CV/JOMAR_NAVARRO_CV_IT.pdf"
 										download="Jomar_Navarro_CV.pdf"
