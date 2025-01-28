c3525118704d786219eb622f42b0e2124e79784e
Jomar-Navarro <Jomarnavarro201@gmail.com>
2025-01-28 18:30:12 +0100

    feat: add i18next translations for Loader component in English and Italian

--- src/assets/components/Loader.jsx
+++ src/assets/components/Loader.jsx
@@ -1,13 +1,17 @@
+import { useTranslation } from "react-i18next";
+
 import styles from "../style/Loader.module.scss";
 
 export default function Loader() {
+	const { t } = useTranslation(); // Hook per la traduzione
+
 	return (
 		<>
 			<div className="container">
 				<div className="d-flex justify-content-center align-items-center">
 					<div className={styles.terminalLoader}>
 						<div className={styles.terminalHeader}>
-							<div className={styles.terminalTitle}>Status</div>
+							<div className={styles.terminalTitle}>{t("loader.status")}</div>
 							<div className={styles.terminalControls}>
 								<div className={`${styles.control} ${styles.close}`}></div>
 								<div className={`${styles.control} ${styles.minimize}`}></div>
@@ -16,7 +20,7 @@ export default function Loader() {
 						</div>
 						<div className={styles.content}>
 							<div className={styles.text}>
-								&lt;div&gt;Loading...&lt;/div&gt;
+								&lt;div&gt;{t("loader.loading")}&lt;/div&gt;
 							</div>
 						</div>
 					</div>
