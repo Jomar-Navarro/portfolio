aa9c1bc8e181f99c6cc655922d97f87ab446651a
Jomar-Navarro <Jomarnavarro201@gmail.com>
2025-03-03 20:14:07 +0100

    refactor: restructure Header component for improved layout and responsiveness

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -60,88 +60,90 @@ export default function Header() {
 	return (
 		<header>
 			<div className="container d-flex justify-content-center my-4">
-				<nav
+				<div
 					className={`${styles.nav} ${
 						scrolled ? styles.scrolled : ""
-					} d-flex justify-content-between align-items-center`}
+					} d-flex justify-content-between align-items-center ${styles.header}`}
 				>
-					<h3 className={styles.title}>Jomar Navarro</h3>
-					<label className={`${styles.hamburger}`}>
-						<input type="checkbox" checked={menuOpen} onChange={toggleMenu} />
-						<svg viewBox="0 0 32 32">
-							<path
-								className={`${styles.line} ${styles.lineTopBottom}`}
-								d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"
-							></path>
-							<path className={styles.line} d="M7 16 27 16"></path>
-						</svg>
-					</label>
+					<nav className="w-100 d-flex align-items-center">
+						<h3 className={styles.title}>Jomar Navarro</h3>
+						<label className={`${styles.hamburger}`}>
+							<input type="checkbox" checked={menuOpen} onChange={toggleMenu} />
+							<svg viewBox="0 0 32 32">
+								<path
+									className={`${styles.line} ${styles.lineTopBottom}`}
+									d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"
+								></path>
+								<path className={styles.line} d="M7 16 27 16"></path>
+							</svg>
+						</label>
 
-					<div
-						className={`${styles.navLinks} ${menuOpen ? styles.active : ""}`}
-					>
-						<ul className="nav">
-							<li className="nav-item">
-								<NavLink
-									to="/"
-									className={({ isActive }) =>
-										isActive
-											? `${styles["nav-link"]} ${styles.isActive}`
-											: `${styles["nav-link"]}`
-									}
-								>
-									{t("header.home")}
-								</NavLink>
-							</li>
-							<li className="nav-item">
-								<NavLink
-									to="/about"
-									className={({ isActive }) =>
-										isActive
-											? `${styles["nav-link"]} ${styles.isActive}`
-											: `${styles["nav-link"]}`
-									}
-								>
-									{t("header.about")}
-								</NavLink>
-							</li>
-							<li className="nav-item">
-								<NavLink
-									to="/projects"
-									className={({ isActive }) =>
-										isActive
-											? `${styles["nav-link"]} ${styles.isActive}`
-											: `${styles["nav-link"]}`
-									}
-								>
-									{t("header.projects")}
-								</NavLink>
-							</li>
-						</ul>
-					</div>
+						<div
+							className={`${styles.navLinks} ${menuOpen ? styles.active : ""}`}
+						>
+							<ul className="nav">
+								<li className="nav-item">
+									<NavLink
+										to="/"
+										className={({ isActive }) =>
+											isActive
+												? `${styles["nav-link"]} ${styles.isActive}`
+												: `${styles["nav-link"]}`
+										}
+									>
+										{t("header.home")}
+									</NavLink>
+								</li>
+								<li className="nav-item">
+									<NavLink
+										to="/about"
+										className={({ isActive }) =>
+											isActive
+												? `${styles["nav-link"]} ${styles.isActive}`
+												: `${styles["nav-link"]}`
+										}
+									>
+										{t("header.about")}
+									</NavLink>
+								</li>
+								<li className="nav-item">
+									<NavLink
+										to="/projects"
+										className={({ isActive }) =>
+											isActive
+												? `${styles["nav-link"]} ${styles.isActive}`
+												: `${styles["nav-link"]}`
+										}
+									>
+										{t("header.projects")}
+									</NavLink>
+								</li>
+							</ul>
+						</div>
 
-					{/* Dropdown delle lingue con le icone */}
-					<Dropdown className={styles.languageSwitcher}>
-						<Dropdown.Toggle variant="" id={styles.languageDropdown}>
-							{getFlagIcon(currentLanguage)}
-						</Dropdown.Toggle>
+						{/* Dropdown delle lingue con le icone */}
+						<Dropdown className={styles.languageSwitcher}>
+							<Dropdown.Toggle variant="" id={styles.languageDropdown}>
+								{getFlagIcon(currentLanguage)}
+							</Dropdown.Toggle>
 
-						<Dropdown.Menu className={styles.dropdownMenu}>
-							<Dropdown.Item
-								className={styles.dropdownItem}
-								onClick={() => changeLanguage("en")}
-							>
-								<span className="fi fi-sh"></span>
-							</Dropdown.Item>
-							<Dropdown.Item
-								className={styles.dropdownItem}
-								onClick={() => changeLanguage("it")}
-							>
-								<span className="fi fi-it"></span>
-							</Dropdown.Item>
-						</Dropdown.Menu>
-					</Dropdown>
-				</nav>
+							<Dropdown.Menu className={styles.dropdownMenu}>
+								<Dropdown.Item
+									className={styles.dropdownItem}
+									onClick={() => changeLanguage("en")}
+								>
+									<span className="fi fi-sh"></span>
+								</Dropdown.Item>
+								<Dropdown.Item
+									className={styles.dropdownItem}
+									onClick={() => changeLanguage("it")}
+								>
+									<span className="fi fi-it"></span>
+								</Dropdown.Item>
+							</Dropdown.Menu>
+						</Dropdown>
+					</nav>
+				</div>
 			</div>
 		</header>
 	);
