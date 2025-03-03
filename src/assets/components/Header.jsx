52e31b20660cbc920b27b352b3d0afb10bd80992
Jomar-Navarro <Jomarnavarro201@gmail.com>
2025-03-03 20:23:48 +0100

    refactor: enhance Header component layout for better alignment and responsiveness

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -65,18 +65,24 @@ export default function Header() {
 						scrolled ? styles.scrolled : ""
 					} d-flex justify-content-between align-items-center ${styles.header}`}
 				>
-					<nav className="w-100 d-flex align-items-center">
-						<h3 className={styles.title}>Jomar Navarro</h3>
-						<label className={`${styles.hamburger}`}>
-							<input type="checkbox" checked={menuOpen} onChange={toggleMenu} />
-							<svg viewBox="0 0 32 32">
-								<path
-									className={`${styles.line} ${styles.lineTopBottom}`}
-									d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"
-								></path>
-								<path className={styles.line} d="M7 16 27 16"></path>
-							</svg>
-						</label>
+					<nav className="w-100 d-flex">
+						<div className="d-flex align-items-center justify-content-between w-100">
+							<h3 className={styles.title}>Jomar Navarro</h3>
+							<label className={`${styles.hamburger}`}>
+								<input
+									type="checkbox"
+									checked={menuOpen}
+									onChange={toggleMenu}
+								/>
+								<svg viewBox="0 0 32 32">
+									<path
+										className={`${styles.line} ${styles.lineTopBottom}`}
+										d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"
+									></path>
+									<path className={styles.line} d="M7 16 27 16"></path>
+								</svg>
+							</label>
+						</div>
 
 						<div
 							className={`${styles.navLinks} ${menuOpen ? styles.active : ""}`}
